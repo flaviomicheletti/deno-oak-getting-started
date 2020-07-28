98fe0b7d1627fdf4a6c66c821dbddf3a300683dd
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-27 22:14:50 -0300

    server 3 and 4

---
+++ server-v4.ts
@@ -0,0 +1,24 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { green, yellow } from "https://deno.land/std@0.53.0/fmt/colors.ts";
+
+const app = new Application();
+const port: number = 8080;
+
+const router = new Router();
+
+router.get("/", ({ response }: { response: any }) => {
+  response.body = {
+    message: "hello world",
+  };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  console.log(`${yellow("Listening on:")} ${green(url)}`);
+});
+
+await app.listen({ port });
