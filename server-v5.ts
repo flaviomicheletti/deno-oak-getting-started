5221fb87d1b4243b06ca4eb9489437d38568563f
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-08-16 17:21:01 -0300

    mais um exemplo extraido da documentação

---
+++ server-v5.ts
@@ -0,0 +1,28 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const port: number = 8080;
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+// Hello World!
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+console.log("running on port ", port);
+
+await app.listen({ port });
